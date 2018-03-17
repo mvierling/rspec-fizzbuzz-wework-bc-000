75686322f592b51acf6b94e287a3641cfd9a57a9
Matt Vierling <matt.vierling@wework.com>
2018-03-17 19:49:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(int)
   if int % 5 == 0 and int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    "FizzBuzz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
   elsif int % 3 == 0 # if the number int is divisible by 3
